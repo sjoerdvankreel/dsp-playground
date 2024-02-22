9b78a1fb485101c415e3222df7874e749872a87f
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-02-22 23:30:34 +0100

    add wisdom

--- src/Filters/TestFilter.cs
+++ src/Filters/TestFilter.cs
@@ -14,7 +14,13 @@
             // In the sence of the analog prototype it's better to put the gain before the integrator,
             // because then the integrator will smooth the jumps and further artifacts arising
             // out of the cutoff modulation
-            
+
+            // More stuff from the book:
+            // Actually, the number of
+            // poles is always equal to the order of the filter or (which is the same) to the
+            // number of integrators in the filter. Therefore it is common, instead of e.g.a
+            // 4th - order filter to say a 4-pole filter
+
             // Well, that's another thing I *actually* understand.
             // Who knows i might have a shot at this stuff in a couple years.
             _integrate += x * whatIsThis;
