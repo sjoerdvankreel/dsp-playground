4f98e3085831970234cbf6016b0d125972c9b2a0
sjoerdvankreel <sjoerdvankreel@gmail.com>
2024-02-22 23:02:56 +0100

    document whatever train of thought

--- src/Filters/TestFilter.cs
+++ src/Filters/TestFilter.cs
@@ -9,6 +9,14 @@
         {
             // higher values of this correspond to increased LPF cutoff
             float whatIsThis = 500;
+
+            // Vadim:
+            // In the sence of the analog prototype it's better to put the gain before the integrator,
+            // because then the integrator will smooth the jumps and further artifacts arising
+            // out of the cutoff modulation
+            
+            // Well, that's another thing I *actually* understand.
+            // Who knows i might have a shot at this stuff in a couple years.
             _integrate += x * whatIsThis;
             _s++;
             return _integrate / _s;
